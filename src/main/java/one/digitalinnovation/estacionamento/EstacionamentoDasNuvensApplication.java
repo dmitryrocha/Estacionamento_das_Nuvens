5ab2946a4b4bf98e0db8a5d96c90858c594a9a18
Dmitry Rocha <dmrosilva@tjba.jus.br>
2022-08-09 18:28:22 -0300

    Swagger using OpenAPI added

--- src/main/java/one/digitalinnovation/estacionamento/EstacionamentoDasNuvensApplication.java
+++ src/main/java/one/digitalinnovation/estacionamento/EstacionamentoDasNuvensApplication.java
@@ -1,9 +1,12 @@
 package one.digitalinnovation.estacionamento;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Estacionamento nas Nuvens API", version = "0.0.1-SNAPSHOT", description = "API criada como parte do bootcamp Spring Boot Experience"))
 public class EstacionamentoDasNuvensApplication {
 
     public static void main(String[] args) {
