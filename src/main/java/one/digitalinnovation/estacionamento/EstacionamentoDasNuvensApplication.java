6c5023efd082841b6496d5422c7eb33982fec8b5
Dmitry Rocha <dmrosilva@tjba.jus.br>
2022-08-09 11:00:57 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovation/estacionamento/EstacionamentoDasNuvensApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.estacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamentoDasNuvensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstacionamentoDasNuvensApplication.class, args);
+    }
+
+}
